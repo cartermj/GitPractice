b496a89e4475a6969d36d76503c3713958c50f0e
Jadennl <jnleonard@hotmail.com>
2019-05-21 12:29:08 -0400

    Fixed code

--- gitPractice/src/gitPractice.java
+++ gitPractice/src/gitPractice.java
@@ -1,6 +1,18 @@
-public class gitPractice					{
-public static void main(int int1, int int2)	{
-double avg = (int1 + int2)/2				;
-System.out.println(avg)						;
-											}
-											}
+import java.util.Scanner;
+
+public class gitPractice {
+    //FIX: this is some disgusting formatting.
+    //FIX: made main String[] args
+    //FIX: Imported Scanner
+    public static void main( String[] args )	{
+        Scanner s = new Scanner( System.in );
+        int int1,int2;
+        System.out.println("Give me a number");
+        int1 = s.nextInt();
+        System.out.println("Give me another number");
+        int2 = s.nextInt();
+        double avg = (int1 + int2)/2;
+        System.out.println("Average: " + avg);
+        s.close();
+    }
+}
